16be83fa7131be4a8f6c122003d8eb3402e6b247
WEITAO JIANG <weitaoj@outlook.com>
2025-02-11 02:55:43 +1100

    Update MovieList

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import { MovieContext } from "../context/MovieContext";
 import MovieCard from "./MovieCard";
 import SortFilter from "./SortFilter";
