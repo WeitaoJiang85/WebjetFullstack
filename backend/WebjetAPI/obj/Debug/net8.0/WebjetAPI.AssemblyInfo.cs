877209b8d04ecacd7a5aca249635840da2763d49
WEITAO JIANG <weitaoj@outlook.com>
2025-02-16 20:22:45 +1100

    Update docker file

--- backend/WebjetAPI/obj/Debug/net8.0/WebjetAPI.AssemblyInfo.cs
+++ backend/WebjetAPI/obj/Debug/net8.0/WebjetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebjetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36aa126d1946ec0132d37a9340a9c69f109f3483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121d18202f669c04c8d3f47e86bdb0d595711c37")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebjetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebjetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
